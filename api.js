2487f3f9a99e1dbfc2a92373ae1aeb91393af8c1
JimmyVazz <cjvasquez_98@hotmail.com>
2019-07-14 11:44:15 -0500

    Arreglado

--- api.js
+++ api.js
@@ -1,32 +1,19 @@
-const http =  require('https');
 
-app.get('/api/', (req, res, next) =>{
-
-  var apiKey = "NyQnVHxBny2NGD9OMdwwu4l9dRyPPFCg8MPlD51T" 
-  var client = "dde8275bfe3a4120ab04f321dcd6213e"
-  var user = "TEAM2"
-  var pass = "e273cd6876774506965e58F563C94e32"  
-
-  let url = "https://s0kg6zbefh.execute-api.us-east-1.amazonaws.com/dev/v1/sandbox/clients/9094578/profile" ; // + req.params.term + "/standings";
-
-  var options = {
-    host: url,
-    method: 'GET',
-    headers: {
-      'X-api-key': apiKey,
-      'X-User': user,
-      'X-Client': client,
-      'X-Password': pass
-    }
-  };
-  
-  var getReq = https.request(options,function(res){
-      console.log(res.body);
-      res.on('data',(data) => {
-          console.log(data);
-      });
-  });
-
-  getReq.end();
-
-})
+    let clientProfile = {
+      "name": "LUIS HACKA3 TEST",
+      "clientNumber": "9094578",
+      "complementaryName": "",
+      "debitCardIndicator": "",
+      "branch": "5",
+      "onboardingDate": "13/08/18",
+      "rfc": "MEML8603025NV",
+      "publicityCode": "Y",
+      "language": "ESP",
+      "nationality": "MX",
+      "birthDate": "02/03/86",
+      "civilStatus": "S",
+      "gender": "M",
+      "homePhone": "5557217478",
+      "officePhone": "5557217478",
+      "curp": ""
+  }
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router  = express.Router();
 const https =  require('https');
-
+const {objetoFeik} = require('../api')
 
 
 /* GET home page */
@@ -26,34 +26,7 @@ router.get('/home/pymes/', (req, res) => {
 })
 
 router.get('/api/', (req, res, next) =>{
-
-  var apiKey = "NyQnVHxBny2NGD9OMdwwu4l9dRyPPFCg8MPlD51T" 
-  var client = "dde8275bfe3a4120ab04f321dcd6213e"
-  var user = "TEAM2"
-  var pass = "e273cd6876774506965e58F563C94e32"  
-
-  let url = "https://s0kg6zbefh.execute-api.us-east-1.amazonaws.com/dev/v1/sandbox/clients/9094578/profile" ; // + req.params.term + "/standings";
-
-  var options = {
-    host: url,
-    method: 'GET',
-    headers: {
-      'X-api-key': apiKey,
-      'X-User': user,
-      'X-Client': client,
-      'X-Password': pass
-    }
-  };
-  
-  var getReq = https.request(options,(res, err) => {
-      console.log(res.body)
-      .then(data => {
-          console.log(data);
-      }).catch(e=>res.render('error'))
-  });
-
-  getReq.end();
-
+  res.json({objetoFeik})
 })
 
 module.exports = router;
